30441a180b6213198caff710261e868df14c5965
Sulaimanb1012 <9024289@student.tcrmbo.nl>
2026-03-12 09:38:08 +0100

    opdracht-06

--- src/opdracht-06/ProfileCard.test.jsx
+++ src/opdracht-06/ProfileCard.test.jsx
@@ -12,21 +12,30 @@ describe('ProfileCard', () => {
 
   test('toont de leeftijd', () => {
     // TODO: Render ProfileCard met name="Piet" en age={30}
+    render(<ProfileCard name="Piet" age={30} />);
+    
     // TODO: Check of tekst "Leeftijd: 30" op het scherm staat
+    const ageText = screen.getByText('Leeftijd: 30');
+    expect(ageText).toBeInTheDocument();
+    
     // Hint: gebruik screen.getByText()
-    expect(true).toBe(false); // Deze test faalt! Vervang met je eigen test
+    // expect(true).toBe(false); // Deze test faalt! Vervang met je eigen test
   });
 
-  test('toont bio als die er is', () => {
-    // TODO: Render ProfileCard met bio="Ik hou van programmeren"
+  test('toont de bio als die aanwezig is', () => {
+    render(<ProfileCard name="Piet" age={30} bio="ik hou van programmeren" />);
     // TODO: Check of die tekst op het scherm staat
-    expect(true).toBe(false); // Deze test faalt! Vervang met je eigen test
+    const bioText = screen.getByText('ik hou van programmeren');
+    expect(bioText).toBeInTheDocument();
   });
 
   test('toont fallback tekst als bio ontbreekt', () => {
     // TODO: Render ProfileCard ZONDER bio prop
+    render(<ProfileCard name="Piet" age={30} />);
+    
     // TODO: Check of "Geen bio beschikbaar" op het scherm staat
-    expect(true).toBe(false); // Deze test faalt! Vervang met je eigen test
+    const fallbackText = screen.getByText('Geen bio beschikbaar');
+    expect(fallbackText).toBeInTheDocument();
   });
 
 });
