9200c998b20bf608ed381e20d005b7ad4f5bb889
Sulaimanb1012 <9024289@student.tcrmbo.nl>
2026-03-12 09:44:42 +0100

    opdracht 7

--- src/opdracht-07/LikeButton.test.jsx
+++ src/opdracht-07/LikeButton.test.jsx
@@ -11,25 +11,41 @@ describe('LikeButton', () => {
 
   test('verhoogt likes bij klik', () => {
     // TODO: Render LikeButton met initialLikes={0}
+    render(<LikeButton initialLikes={0} />);
     // TODO: Vind de button (Hint: screen.getByRole('button'))
+    const button = screen.getByRole('button');
     // TODO: Klik op de button (Hint: fireEvent.click(button))
+    fireEvent.click(button);
     // TODO: Check of "1 likes" op het scherm staat
-    expect(true).toBe(false); // Deze test faalt! Vervang met je eigen test
+    const likesText = screen.getByText('1 likes');
+    expect(likesText).toBeInTheDocument();
+   // expect(true).toBe(false); // Deze test faalt! Vervang met je eigen test
   });
 
   test('verlaagt likes bij tweede klik', () => {
     // TODO: Render LikeButton
+    render(<LikeButton initialLikes={0} />);
     // TODO: Klik 2x op de button
+    const button = screen.getByRole('button');
+    fireEvent.click(button);
+    fireEvent.click(button);
     // TODO: Check of we terug zijn bij "0 likes"
-    expect(true).toBe(false); // Deze test faalt! Vervang met je eigen test
+    const likesText = screen.getByText('0 likes');
+    expect(likesText).toBeInTheDocument();
+    // expect(true).toBe(false); // Deze test faalt! Vervang met je eigen test
   });
 
   test('toont rood hartje als geliked', () => {
     // TODO: Render LikeButton
+    render(<LikeButton initialLikes={0} />);
     // TODO: Klik op de button
+    const button = screen.getByRole('button');
+    fireEvent.click(button);
     // TODO: Check of de button tekst "❤️ Like" bevat
     // Hint: screen.getByText(/❤️ Like/)
-    expect(true).toBe(false); // Deze test faalt! Vervang met je eigen test
+    const likedButton = screen.getByText(/❤️ Like/);
+    expect(likedButton).toBeInTheDocument();
+    // expect(true).toBe(false); // Deze test faalt! Vervang met je eigen test
   });
 
 });
